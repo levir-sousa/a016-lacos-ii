7d4677e4a54115501c269a98665000ae25a545aa
levir-sousa <levysouza2014@gmail.com>
2022-08-18 18:44:12 -0300

    exercicio 3

---
+++ exercicio3/script.js
@@ -0,0 +1,7 @@
+const maioresPaises = ["Rússia", "Canadá", "China", "EUA", "Brasil"];
+let ranking=0;
+
+for(const paises of maioresPaises){
+    ranking++;
+    console.log(`"${ranking} - ${paises}"`);
+}
